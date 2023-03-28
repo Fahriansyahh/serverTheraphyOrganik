96cd3ed1da8763a9a5bf803bf2a9843042ed9ce7
unknown <fahrumfahriansyah1@gmail.com>
2023-03-28 09:46:09 -0700

    menambahkan data

--- src/Routes/User.js
+++ src/Routes/User.js
@@ -4,7 +4,7 @@ const { body } = require('express-validator')
 
 const User = require("../Controllers/ControlUser")
 
-route.post("/Created", [body("FullName").isUppercase().withMessage("Full Name harus huruf Besar"), body("Password").isLength({ min: 3, max: 20 }).withMessage("Password minimal 3"), body("Email").isEmail().withMessage("ini bukan email"), body("NoHp").isMobilePhone('id-ID').withMessage("Pastikan nomor hp anda +62")], User.Created)
+route.post("/Created", [body("FullName").isLength({ min: 1, max: 100 }).withMessage("Fullname tidak ada"), body("Password").isLength({ min: 3, max: 20 }).withMessage("Password minimal 3"), body("Email").isEmail().withMessage("ini bukan email"), body("NoHp").isMobilePhone('id-ID').withMessage("Pastikan nomor hp anda +62")], User.Created)
 
 route.get("/Auth", User.Auth)
 route.get("/GetById/:id", User.GetById)
@@ -12,6 +12,6 @@ route.get("/GetAll", User.GetAll)
 route.put("/KeyPassword", User.KeyPassword)
 route.put("/newAcount/:id", [body("Email").isEmail().withMessage("ini bukan email"), body("Password").isLength({ min: 3, max: 20 }).withMessage("Password minimal 3")], User.newAcount)
 route.put("/updatePesan/:id", User.Update)
-route.put("/updateUser/:id", [body("FullName").isUppercase().withMessage("Full Name harus huruf Besar"), body("Email").isEmail().withMessage("ini bukan email"), body("NoHp").isMobilePhone('id-ID').withMessage("Pastikan nomor hp anda +62")], User.updateUser)
+route.put("/updateUser/:id", [body("FullName").isLength({ min: 1, max: 100 }).withMessage("Fullname tidak ada"), body("Email").isEmail().withMessage("ini bukan email"), body("NoHp").isMobilePhone('id-ID').withMessage("Pastikan nomor hp anda +62")], User.updateUser)
 route.delete("/DeleteById/:id", User.DeleteById)
 module.exports = route
